8c3edb2395ed95533809e028ac73452503d1458a
Rithor <vitaliy.andrianov@icloud.com>
2019-10-18 18:04:38 +0500

    add url request to weather api; parce JSON, and create Weather Data model

---
+++ 13.new.Clima-iOS13/Model/WeatherData.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherData.swift
+//  ClimaiOS13
+//
+//  Created by Vitaliy on 18.10.2019.
+//  Copyright © 2019 VitaliyAndrianov. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let humidity: Int
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let description: String
+}
+
--- 13.new.Clima-iOS13/Model/WeatherManager.swift
+++ 13.new.Clima-iOS13/Model/WeatherManager.swift
@@ -11,10 +11,29 @@ import Foundation
 struct WeatherManager {
     
     let weatherURL =
-    "http://api.openweathermap.org/data/2.5/weather?appid=\(Security.appid)&units=metric"
+    "https://api.openweathermap.org/data/2.5/weather?appid=\(Security.appid)&units=metric"
     
     func fetchWeather(cityName: String) {
         let finalURL = "\(weatherURL)&q=\(cityName)"
-        print(finalURL)
+        performRequestWith(stringURL: finalURL)
+    }
+    
+    private func performRequestWith(stringURL: String) {
+        guard let url = URL(string: stringURL) else { return } //TODO: alert if return
+        URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
+            guard error == nil else { return } //TODO: alert if return
+            guard let weatherData = data else { return } //TODO: alert if return
+            self.parceJSON(data: weatherData)
+        }.resume()
+    }
+    
+    private func parceJSON(data: Data) {
+        do {
+            let decodableData = try JSONDecoder().decode(WeatherData.self, from: data)
+            print(decodableData.main.temp)
+        } catch {
+            print(error) //TODO: show user alert
+        }
+        
     }
 }
